ef72bf7ddfb09042bfefc078ed043319c59c7bbe
Andrew Liseczkiy <45668739+Liseckij2@users.noreply.github.com>
2021-11-01 02:04:07 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,36 +1,38 @@
-
-let containerEl = document.querySelector("#container"),
-  inputEl = document.querySelector("#container__input"),
-  buttonEl = document.querySelector("#container__button"),
-  toDoListEl = document.querySelector("#todo");
+let inputEl = document.querySelector(".container__input"),
+  buttonEl = document.querySelector(".container__button"),
+  toDoListEl = document.querySelector(".todo");
 
 buttonEl.addEventListener("click", () => {
   let createDiv = document.createElement("div");
-  createDiv.innerHTML =
-    '<input type="checkbox" name="" class="todo__checkbox">';
-  createDiv.innerHTML += `<span class="todo__span">${inputEl.value}</span>`;
-  createDiv.innerHTML += '<button class="todo__button">Delete</button>';
+  createDiv.className = "note";
+
+  let createInput = document.createElement("input");
+  createInput.setAttribute("type", "checkbox");
+  createInput.setAttribute("class", "checkbox_checked");
+  createDiv.appendChild(createInput);
+
+  let createSpan = document.createElement("span");
+  createSpan.textContent = `${inputEl.value}`;
+  createDiv.appendChild(createSpan);
+
+  let creareButton = document.createElement("input");
+  creareButton.setAttribute("type", "button");
+  creareButton.setAttribute("value", "DELETE");
+  creareButton.className = "container__button";
+  createDiv.appendChild(creareButton);
+
   toDoListEl.appendChild(createDiv);
 
-  // <input type="checkbox" name="" class="todo__checkbox">
-  //   <span class="todo__span">${inputEl.value}</span>
-  //   <button class="todo__button">Delete</button>
-
-  let todoCheckboxEl = document.querySelector(".todo__checkbox"),
-    todoBtnEl = document.querySelector(".todo__button"),
-    todoSpanEl = document.querySelector(".todo__span");
-
-  todoCheckboxEl.addEventListener("click", () => {
-    if (todoCheckboxEl.checked === true) {
-      todoSpanEl.classList.add("class");
-    } else if (todoCheckboxEl.checked === false) {
-      todoSpanEl.classList.remove("class");
+  createDiv.addEventListener("click", () => {
+    console.log("click");
+    if (createInput.checked === true) {
+      createSpan.classList.add("class");
+    } else if (createInput.checked === false) {
+      createSpan.classList.remove("class");
     }
-    // console.log("click");
   });
-  todoBtnEl.addEventListener('click', () => {
-    toDoListEl.removeChild(createDiv)
-  })
-
+  creareButton.addEventListener("click", () => {
+    toDoListEl.removeChild(createDiv);
+  });
   inputEl.value = "";
 });
